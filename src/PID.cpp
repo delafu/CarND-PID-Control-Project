0a7461762ce634ea33208f7bc7df06f1430bc62d
Jose Antonio de la Fuente Sanz <delafu@MacBook-Air-de-Jose.local>
2017-11-27 23:56:28 +0100

    Primera Mac

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,9 +11,13 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    this->Kp = Kp;
+    this->Ki = Ki;
+    this->Kd = Kd;
 }
 
 void PID::UpdateError(double cte) {
+
 }
 
 double PID::TotalError() {
